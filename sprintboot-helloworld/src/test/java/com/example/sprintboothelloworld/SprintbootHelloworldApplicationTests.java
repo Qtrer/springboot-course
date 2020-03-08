f373eb1ed8e1e4d9545458cab34e0a761238beb6
Qtrer <2537943540@qq.com>
2020-03-08 18:29:06 +0800

    springboot helloworld初始化

---
+++ sprintboot-helloworld/src/test/java/com/example/sprintboothelloworld/SprintbootHelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sprintboothelloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintbootHelloworldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
