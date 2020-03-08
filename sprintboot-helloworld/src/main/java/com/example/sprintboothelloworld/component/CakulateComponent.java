681fbb85c3cb46f6363e8b2e499d927c9afeb4eb
Qtrer <2537943540@qq.com>
2020-03-08 19:05:43 +0800

    springboot helloworld完成

---
+++ sprintboot-helloworld/src/main/java/com/example/sprintboothelloworld/component/CakulateComponent.java
@@ -0,0 +1,10 @@
+package com.example.sprintboothelloworld.component;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class CakulateComponent {
+    public int add(int x, int y){
+        return x + y;
+    }
+}
---
+++ sprintboot-helloworld/src/test/java/com/example/sprintboothelloworld/component/CakulateComponentTest.java
@@ -0,0 +1,16 @@
+package com.example.sprintboothelloworld.component;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class CakulateComponentTest {
+    @Autowired
+    private CakulateComponent cakulateComponent;
+    @Test
+    public void test_add(){
+        Assertions.assertEquals(2, cakulateComponent.add(1, 1));
+    }
+}
